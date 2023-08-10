da1c293b2c0b2e03ed2f6c14850274518a37842d
arshi45 <arshdeep0220@gmail.com>
2023-08-11 04:59:23 +0530

    Navbar

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,8 +1,27 @@
 
-export const navbar = () => {
+import { useState } from 'react';
+import styles from './navbar.module.css';
+import Link from 'next/link';
+const Navbar = (props:any) => {
+
+    const [popup,setPopUp] = useState(false);
+
+    const handlePopUp = () => {
+        setPopUp(!popup);
+    }
     return (
-        <div>
-            
+        <div className={styles.navbar}>
+            <img src='images/Logo.png'/>
+            <div className={styles.profile} onClick={handlePopUp}>
+                <img src='txt' className={styles.profilePhoto}/>
+                {popup?
+                <div className={styles.popUp}>   
+                    <Link href="/Profile">Profile</Link>
+                    <hr></hr>
+                    <button>LogOut</button>
+                </div>:null}
+            </div>
         </div>
     )
 }
+export default Navbar;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import Navbar from "@/Components/Navbar";
+import styles from "./page.module.css";
 
-export default function Home() {
-  return <div>
+export default function Page() {
+  return <div className={styles.main}>
     <div>
+            <Navbar/>
             <Link href="/pages">Create a Post</Link>
             <Link href="/home">Home</Link>
         </div>
