3f27244b14b85d0f67706a51dd719e0b1bbbe374
karanthakur13 <89067270+karanthakur13@users.noreply.github.com>
2023-08-11 13:13:00 +0530

    final changes

--- client/src/Components/Blogcard.jsx
+++ client/src/Components/Blogcard.jsx
@@ -1,28 +1,38 @@
 "use client";
 import React from "react";
 import styles from "./blogCard.module.css";
-import axios from 'axios';
+import axios from "axios";
 import { log } from "console";
+import { useRewardContext } from "../context/reward";
 
 const BlogCard = (props) => {
-
-  const handleCoins = async (e) =>  {
-    console.log(props.blogData);
+  const { Reward, contract } = useRewardContext();
+  const handleCoins = async (e) => {
+    /* console.log(props.blogData);
     const val = e.target.value;
-    await axios.post("http://localhost:5001/api/scoreUpdate",{
-      metaID:props.blogData.publisher,
-      blogID:props.blogData.ID,
-      vote:val
-    },{
-      headers:{
-        "Content-Type":"application/json"
-      },
-    }).then((res) => {
-      res.json();
-    }).then(data => {
-      console.log(data);
-    });
-  }
+    await axios
+      .post(
+        "http://localhost:5001/api/scoreUpdate",
+        {
+          metaID: props.blogData.publisher,
+          blogID: props.blogData.ID,
+          vote: val,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      }); */
+
+    const data = await Reward(props.blogData.publisher, 1);
+  };
 
   return (
     <div className={styles.blogCard}>
@@ -32,8 +42,10 @@ const BlogCard = (props) => {
         </div>
         <h2>{props.blogData.title}</h2>
         <p>{props.blogData.text}</p>
-        <button onClick={handleCoins} value={1}>upvote</button>
-        <button onClick={handleCoins} value={0}>downvote</button>
+        <button onClick={handleCoins} value={1}>
+          upvote
+        </button>
+        <button value={0}>downvote</button>
       </div>
     </div>
   );
